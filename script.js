6f0f543d9355e449b64cdb7acf5a66190c5e44d5
padmasrimallela2724 <168058212+padmasrimallela2724@users.noreply.github.com>
2026-08-28 11:52:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function goToPage2() {
+    window.location.href = "page2.html";
+}
+
+function goToPage3() {
+    window.location.href = "page3.html";
+}
+
+function showSurprise() {
+    document.getElementById("surpriseMessage").classList.remove("hidden");
+}
